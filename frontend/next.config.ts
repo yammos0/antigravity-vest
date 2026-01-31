ef3e9de88f18285966588dd2b973ad80d59e781a
yammos0 <maximokx1@gmail.com>
2026-01-31 20:03:44 +0200

    Fix: Disable local proxy for Vercel deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,14 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/api/python/:path*',
-        destination: 'http://127.0.0.1:8000/api/v1/:path*',
-      },
-    ]
-  },
+  /*
+    async rewrites() {
+      return [
+        {
+          source: '/api/python/:path*',
+          destination: 'process.env.NEXT_PUBLIC_API_URL/:path*', // Needs real Env var in Prod
+        },
+      ]
+    },
+  */
 };
 
 export default nextConfig;
